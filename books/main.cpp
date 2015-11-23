feccd3863ae78a8143127b2899e5472e3cca4f46
kellydavid <kellydavid178@gmail.com>
2015-11-23 10:22:31 +0000

    Initial Commit

---
+++ books/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  books
+//
+//  Created by David Kelly on 23/11/2015.
+//  Copyright © 2015 David Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
